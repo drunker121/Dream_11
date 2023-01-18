247f8e24911cdbb7623dc6346a4d08fcd4da3461
drunker121 <87312371+drunker121@users.noreply.github.com>
2023-01-18 18:42:08 +0530

    finals

--- back/index.js
+++ back/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    withCredentials: true,
+    credentials: true,
   })
 );
 
--- front/src/components/Success.js
+++ front/src/components/Success.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function 
 () {
   return (
-    <div>
-        Contest Added successfully
+    <div className='bg-black h-screen text-white font-bold text-5xl flex justify-center'>
+       <h1 className='my-80'>Contest Added successfully</h1> 
     </div>
   )
 }
